5b0dfa2c823b4f11b97c070b06b81bad61214046
LitangDingzhen <stupid@neeerd.top>
2025-10-15 13:23:25 +0800

    ..

--- updater.py
+++ updater.py
@@ -40,3 +40,48 @@ if latest_version_json["latest_version"] > version:
 else:
     print("程序已是最新版本")
     exit(0)
+
+update_instructions = []
+for i in range(version + 1, latest_version_json["latest_version"] + 1):
+    print(f"正在获取版本 {i} 的更新...")
+    update_instructions.append(requests.get(f"https://raw.githubusercontent.com/A8Cl233395/FunQQBotUpdater/refs/heads/main/{i}.py", timeout=10).text)
+
+update_instructions_json = []
+for instruction in update_instructions:
+    update_instructions_json.append(yaml.safe_load(instruction))
+print("正在应用更新...")
+
+all_updated_files = []
+all_deleted_files = []
+all_update_codes = []
+for instruction in update_instructions_json:
+    for updated_files in instruction["updated_files"]:
+        if updated_files not in all_updated_files:
+            all_updated_files.append(updated_files)
+    for deleted_files in instruction["deleted_files"]:
+        if deleted_files not in all_deleted_files:
+            all_deleted_files.append(deleted_files)
+        if deleted_files in all_updated_files:
+            all_updated_files.remove(deleted_files)
+    all_update_codes.append(instruction["update_code"])
+
+for file in all_updated_files:
+    print(f"正在更新文件 {file} ...")
+    file_url = f"https://raw.githubusercontent.com/A8Cl233395/FunQQBot/refs/heads/main/{file}"
+    try:
+        file_content = requests.get(file_url, timeout=10).text
+    except requests.Timeout:
+        print("请求超时，请检查网络连接")
+        exit(1)
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(file_content)
+
+for file in all_deleted_files:
+    print(f"正在删除文件 {file} ...")
+    import os
+    if os.path.exists(file):
+        os.remove(file)
+
+print("正在执行更新代码...")
+for code in all_update_codes:
+    exec(code)
